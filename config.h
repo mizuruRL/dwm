34ca87f866a01faa81b30eb719854e638205d51a
mizuruRL <37560410+mizuruRL@users.noreply.github.com>
2020-07-05 05:58:40 +0100

    change keyshortcuts to giga chad vim-like binds

--- config.h
+++ config.h
@@ -3,15 +3,15 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 8;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 8;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 12;       /* vertical padding of bar */
-static const int sidepad            = 12;       /* horizontal padding of bar */
+static const int vertpad            = 8;       /* vertical padding of bar */
+static const int sidepad            = 8;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=12" };
 static const char norm_fg[] = "#9191cb";
 static const char norm_bg[] = "#0c0923";
@@ -52,20 +52,20 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "[M]",      monocle },
-//	{ "[@]",      spiral },
-//	{ "[\\]",     dwindle },
-	{ "H[]",      deck },
-	{ "TTT",      bstack },
-//	{ "===",      bstackhoriz },
-	{ "HHH",      grid },
-//	{ "###",      nrowgrid },
-//	{ "---",      horizgrid },
-//	{ ":::",      gaplessgrid },
-	{ "|M|",      centeredmaster },
-//	{ ">M>",      centeredfloatingmaster },
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "",      tile },    /* first entry is default */
+	{ "",      monocle },
+//	{ "",      spiral },
+//	{ "",     dwindle },
+	{ "",      deck },
+	{ "",      bstack },
+//	{ "",      bstackhoriz },
+	{ "",      grid },
+//	{ "",      nrowgrid },
+//	{ "",      horizgrid },
+//	{ "",      gaplessgrid },
+	{ "",      centeredmaster },
+//	{ "",      centeredfloatingmaster },
+	{ "",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
@@ -87,20 +87,16 @@ static const char *dmenucmd[] = { "dmenu_run", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          SHCMD("st") }, 
-	{ MODKEY,                       XK_b, spawn,          SHCMD("firefox") }, 
-	{ MODKEY,                       XK_d, spawn,          SHCMD("discord") }, 
+	{ MODKEY,                       XK_s, spawn,          SHCMD("st") }, 
 	{ MODKEY,                       XK_r, spawn,          SHCMD("ranger") }, 
 	{ 0,                       XK_Print, spawn,          SHCMD("sh ~/Scripts/screenshot") }, 
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_Right,   focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_Left,     focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_Up,     focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_k,   focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_j,     focusstack,     {.i = -1 } },
 //	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 //	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_Left,      setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_Right,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_plus,   incrgaps,       {.i = +1 } },
 	{ MODKEY,                       XK_minus,  incrgaps,       {.i = -1 } },
@@ -119,7 +115,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_g,      togglegaps,     {0} },
 	{ MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
 	{ Mod1Mask,                     XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_BackSpace,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ Mod1Mask,                     XK_1,      setlayout,      {.v = &layouts[0]} },
 	{ Mod1Mask,                     XK_2,      setlayout,      {.v = &layouts[2]} },
 	{ Mod1Mask,                     XK_3,      setlayout,      {.v = &layouts[3]} },
@@ -130,7 +126,7 @@ static Key keys[] = {
 //	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_f,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY,                       XK_s,      toggletag,      {.ui = ~0 } },
+	{ MODKEY,                       XK_s,      tag,      {.ui = ~0 } },
 //	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 //	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 //	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
